862a45d47000d384cda815e509fff70c4ea12008
Steve Pedersen <speder@pacbell.net>
2015-11-17 16:54:39 -0800

    Kebabify!

---
+++ kebab_case/pi.kebab_case.php
@@ -0,0 +1,36 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+$plugin_info = array(
+    'pi_name'        => 'Kebab Case',
+    'pi_version'     => '1.0',
+    'pi_author'      => 'Steve Pedersen',
+    'pi_author_url'  => 'http://www.bluecoastweb.com/',
+    'pi_description' => 'Everybody loves a kebab',
+    'pi_usage'       => Kebab_case::usage()
+);
+
+class Kebab_case {
+    public function __construct() {
+        $text = ee()->TMPL->fetch_param('text');
+        $this->return_data = $this->_kebabify($text);
+    }
+
+    private function _kebabify($text) {
+        return preg_replace('/-$/', '', preg_replace('/\W+/', '-', strtolower($text)));
+    }
+
+    public static function usage() {
+        ob_start();
+?>
+Return the value of text as kebab-case:
+
+  {exp:kebab_case text="What is kebab case?"}
+<?php
+        $buffer = ob_get_contents();
+        ob_end_clean();
+        return $buffer;
+    }
+}
+
+/* End of file pi.kebab_case.php */
+/* Location: /system/expressionengine/third_party/kebab_case/pi.kebab_case.php */
